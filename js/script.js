e5aa9db404b5c7f85f54611545ad3d910e13a42f
Panasenko-p <pvpanasenko@gmail.com>
2022-01-29 10:21:30 +0200

    five

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carusel').slick({
         speed: 1200,
-        prevArrow:'<button type="button" class="slick-prev"> <img src="../icons/Arrow 1 (Stroke).svg" alt="Arrow"></img> </button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="../icons/Arrow 2 (Stroke).svg" alt="Arrow2"></img></button>',
+        prevArrow:'<button type="button" class="slick-prev"> <img src="icons/Arrow 1 (Stroke).svg" alt="Arrow"></img> </button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/Arrow 2 (Stroke).svg" alt="Arrow2"></img></button>',
     });
   });
